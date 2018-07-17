309e54933ce84ae84c8bdb0e12f6ec2467d111dc
NoellaJulianna Morris <nmorris@sp309-196.cs.vassar.edu>
2018-07-17 14:56:42 -0400

    methods till dayListStr fully functional!

--- event.py
+++ event.py
@@ -1,16 +1,16 @@
-from datetime import datetime, date, time
+import datetime
 
 class Event(object):
 
-    def __init__(self, start, time, freq):
+    def __init__(self, start, freq):
         """
         Construct a Event info with the given month, day, and year
         time of day, and frequency of the event
         """
         self.month = start.month
-        self.day = dy
-        self.year = yr
-        self.time = time
+        self.day = start.day
+        self.year = start.year
+        self.time = start.hour,start.minute
         self.freq = freq
 
     def genSeq(self):
@@ -18,28 +18,36 @@ class Event(object):
         Generates a list of the nth day that the event will happen
         depending on its inputed frequency
         """
-        NthDay = [0]
-        for x in range(365/self.freq):
-            NthDay += [NthDay[-1] + self.freq]
+        NthDay = list(range(0,366,self.freq))
         return NthDay
-        #Use the step argument of range
 
-    def dayList(self):
+    def dayListNums(self):
         """
         Generates a list the NUMBERS OF THE DAY OF the week
-        (i.e. Monday = 0, Tuesday = 1 etc)
+        (i.e. Monday = 0, Tuesday = 1, etc.)
         that the event will happen depending on its inputed 
-        frequency
+        frequency.
         """
         NthDay = self.genSeq()
-        daysListNum = []
         daysList = []
         days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
         start = datetime.datetime(self.year,self.month,self.day)
         for x in range(len(NthDay)):
-            nth = timedelta(days=NthDay[x])
-            daysList += date(start+nth).weekday()
-        for x in daysListNum
+            nth = datetime.timedelta(days=NthDay[x])
+            newDate = start + nth
+            daysList += [datetime.date(newDate.year,newDate.month,newDate.day).weekday()]
+        return daysList
+
+    def dayListStr(self):
+        """
+        Generates a list of the days of the week that the event will happen
+        depending on its inputed frequency.
+        """
+        daysList = self.dayListNums()
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        for x in range(len(daysList)):
+            daysList[x] = days[daysList[x]]
+        return daysList
 
             
 
