80dec0d4aec98848a4ae84dcc4b914f04db99829
NoellaJulianna Morris <nmorris@sp309-196.cs.vassar.edu>
2018-07-18 17:25:00 -0400

    schedule now works like a scheduler!!!

--- schedule.py
+++ schedule.py
@@ -3,66 +3,40 @@ import eventgen
 
 class Schedule(object):
     
-    def __init__(self, name, start, freq):
-        self.start = start
+    def __init__(self, event, start=None, end=None):
+        if start == None:
+            self.start = event.start
+        else:
+            self.start = start
+        if end == None:
+            self.end = event.end
+        else:
+            self.end = end
         self.month = start.month
         self.day = start.day
         self.year = start.year
-        self.time = datetime.time(start.hour,start.minute)
-        self.hour = start.hour
-        self.minute = start.minute
-        self.freq = freq
-        self.name = name
-        self.egInfo = eventgen.EventGen(name,start,freq)
+        self.egInfo = [event]
 
     def createSchedule(self):
         """
         Writes a schedule for a given event into a text file
         called schedule.txt
         """
-        f = open("schedule.txt", "w+")
-        NthDays = self.egInfo.genSeq()
-        daysList = self.egInfo.dayListStr()
-        start = datetime.datetime(self.year,self.month,self.day)
-        for x in range(len(daysList)):
-            nth = datetime.timedelta(days=NthDays[x])
-            newDate = start + nth
-            schedDate = datetime.date(newDate.year,newDate.month,newDate.day)
-            f.write('To {} on day {} on the date {} at {}.\r\n'.format(self.name,daysList[x],schedDate,self.time))
-        f.close()
-    
-    def findCyclePoint(self):
-        """
-        Returns the most recent instantiation of an event.
-        """
-        nthNow = datetime.datetime.now() - self.start
-        for x in range(len(self.egInfo.genSeq())):
-            cyclePoint = self.egInfo.genSeq()[x]
-            if nthNow < datetime.timedelta(days=self.freq):
-                return 0
-            elif nthNow < datetime.timedelta(days=cyclePoint):
-                return x-1
-
-    def createHistory(self):
-        """
-        Writes a history of instances of an event into a text 
-        file called history.txt
-        """
-        f = open("history.txt", "w+")
-
-    def resetSchedule(self):
-        """
-        Resets the schedule to have the start date be the next day
-        (Used for when a cycle is broken)
-        """
-
-    def markEventDone(self):
-        """
-        Marks an event done for a certain date and time
-        """
-
-    def checkUndoneEvent(self):
-        """
-        Checks whether there are any events from the last check till now
-        that have not been fulfilled
-        """
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        events = []
+        for x in self.egInfo:
+            if x.start > self.end:
+                continue
+            NthDays = x.genSeq()
+            for i in NthDays:
+                nth = datetime.timedelta(days=i)
+                newDate = x.start + nth
+                fullDate = datetime.datetime(newDate.year,newDate.month,newDate.day,x.hour,x.minute)
+                if fullDate > self.end:
+                    break
+                else:
+                    events += [x.name,fullDate]
+        events.sort()
+        with open("schedule.txt", "w+") as f:
+            for b in events:
+                f.write('To {} on day {} on the date {} at {}.\r\n'.format(b.name,days[b.weekday()],b.date(),b.time()))
