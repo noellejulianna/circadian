e202d1df14d4d55dd3d378b992fb1bd537cc4d25
NoellaJulianna Morris <nmorris@sp309-196.cs.vassar.edu>
2018-07-20 16:35:36 -0400

    added some logic for how editing events is gonna work

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ model schedule
 generate a list of events from model
 call a function to display the week of those events
 
+def editEvent(allEvents,newEvent):
+    """
+    Edits event
+    """
+    for x in allEvents:
+        if x.name == newEvent.name:
+            if x.start != newEvent.start:
+                x.start = newEvent.start
+            if x.end != newEvent.end:
+                x.end = newEvent.end
+            if x.freq != newEvent.freq:
+                x.freq = newEvent.freq
+
 def loadSchedule(self):
     """
     Loads the schedule to the app.
@@ -24,6 +37,11 @@ def main():
     sched = loadSchedule()
     if loadSchedule == 0:
         pass
+    #edit event
+    if doubleclick event:
+        open create event window
+        copy name, streak and lateness and input new event information (start,end,freq)
+        call editEvent([list of all events],newedited event) #will overwrite inputed changes
     
 write pygame program that creates objects
 then picles them
